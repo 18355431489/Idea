f02cb47a57893a6feb9bfd9854596ea938b6daac
tang <1526714262@qq.com>
2020-06-11 11:01:19 +0800

    idea第一次提交

---
+++ src/com/TT1.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @description: TODO
+ * @author: 唐小甫
+ * @create: 2020/6/11 10:58
+ * @version: 1.0
+ */
+public class TT1 {
+    public static void main(String[] args) {
+        System.out.println("hello TT1");
+    }
+}
---
+++ src/com/TT2.java
@@ -0,0 +1,13 @@
+package com;
+
+/**
+ * @description: TODO
+ * @author: 唐小甫
+ * @create: 2020/6/11 10:59
+ * @version: 1.0
+ */
+public class TT2 {
+    public static void main(String[] args) {
+        System.out.println("hello TT2");
+    }
+}
